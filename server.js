3076b73bf3362de4cac43899b16e100ccba3d53e
dhiman28 <erascan.vishal@gmail.com>
2020-09-20 22:00:57 +0530

    Version 1.0

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+var cors = require('cors');
 const app = express();
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use(bodyParser.json())
