5d636df2996b142877dfc9aafc6702ba77e0f0b0
Vishal Dhiman <48185246+vishaldhiman28@users.noreply.github.com>
2020-09-27 12:02:25 +0530

    Update twilio.config.js

--- config/twilio.config.js
+++ config/twilio.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    accountSid: 'AC14ae2cadbf96f176fdb685e57701668d',
-    authToken: '02acfaf2364ec0668763e8f765c1b923',
-    from: '+12162385542',
+    accountSid: '',
+    authToken: '',
+    from: '',
 }
